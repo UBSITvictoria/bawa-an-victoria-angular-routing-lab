9c49589f3f924686df12728e1f8152bb26b9dacd
UBSITvictoria <20239090@s.ubaguio.edu>
2025-03-06 15:11:01 +0800

    WIP: Committing all components (still in progress)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './about/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent, children: [
+      { path: 'profile', component: ProfileComponent }
+  ]},
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/home' }  // Handles unknown paths
+];
