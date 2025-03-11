fcb48b3726669992357d99137856927c263a56ac
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-03-11 16:13:24 +0800

    updated styles

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterModule,RouterOutlet],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
