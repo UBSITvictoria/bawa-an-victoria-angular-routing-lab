f7e85b8ab64419d4f83c2ffe98bffc39fbbaeb52
UBSITvictoria <20239090@s.ubaguio.edu>
2025-03-20 14:49:34 +0800

    Made adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,19 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  pageTitle = 'Angular 19 Data Binding';
+  currentTime = new Date();
   title = 'angular-child-routes-example';
+
+  clickCount= 0;
+  incrementCount(){
+    this.clickCount++;
+  }
+
+  onInput(event:Event){
+    console.log((event.target as HTMLInputElement).value);
+  }
+
   name = '';
   email = '';
   phone_no = '';
